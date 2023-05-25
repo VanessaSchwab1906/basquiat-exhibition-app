b4b256897fb6fc3b247f31acaf1c45d6d40b963c
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-05-25 16:50:29 +0200

    add styles to EventItem

--- src/components/EventItem/EventItem.js
+++ src/components/EventItem/EventItem.js
@@ -11,41 +11,41 @@ export default function EventItem() {
             <span className="event-list-item__month">May</span>
             <div className="event-list-item__day-time-wrapper">
               <span className="event-list-item__day">Thursday</span>
-              <span className="event-list-item__time"> 7pm </span>
+              <span className="event-list-item__time">7pm</span>
+              <div className="event-list-item__text-wrapper">
+                <div className="event-list-item__text">
+                  <h3>
+                    <a className="o-linklist__link">
+                      <span>Opening</span>
+                    </a>
+                  </h3>
+                  <p className="event-list-item__description">
+                    with Julius von Bismarck, Nasan Tur, Böhler & Orendt, Pnar
+                    Öğrenci and „Suddenly Wonderful“
+                  </p>
+                </div>
+                <AddToCalendarButton
+                  name="[Reminder] Test the Add to Calendar Button"
+                  startDate="2023-05-28"
+                  startTime="10:15"
+                  endTime="23:30"
+                  timeZone="America/Los_Angeles"
+                  description="Check out the maybe easiest way to include Add to Calendar Buttons to your web projects:[br]→ [url]https://add-to-calendar-button.com/"
+                  options="'iCal'"
+                  buttonsList
+                  hideTextLabelButton
+                  buttonStyle="round"
+                  lightMode="bodyScheme"
+                  size="8|6|4"
+                />
+              </div>
             </div>
           </div>
           <div className="event-list-item__body">
-            <span class="event-list-item__tag">Vernissage</span>
-            <h3>
-              <a className="o-linklist__link">
-                {" "}
-                <span>Opening</span>
-              </a>
-            </h3>
-            <div className="event-list-item__text">
-              <p>
-                with Julius von Bismarck, Nasan Tur, Böhler &amp;
-                Orendt,&nbsp;Pnar Öğrenci und „Suddenly Wonderful“
-              </p>
-            </div>
+            {/* <span className="event-list-item__tag">Vernissage</span> */}
           </div>
         </li>
       </div>
-
-      <AddToCalendarButton
-        name="[Reminder] Test the Add to Calendar Button"
-        startDate="2023-05-28"
-        startTime="10:15"
-        endTime="23:30"
-        timeZone="America/Los_Angeles"
-        description="Check out the maybe easiest way to include Add to Calendar Buttons to your web projects:[br]→ [url]https://add-to-calendar-button.com/"
-        options="'iCal'"
-        buttonsList
-        hideTextLabelButton
-        buttonStyle="round"
-        lightMode="bodyScheme"
-        size="8|6|4"
-      ></AddToCalendarButton>
     </>
   );
 }
