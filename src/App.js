4562ddd4715550b84aa39ddd995d2c64a047ed36
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-05-30 16:35:55 +0200

    add and styled canvas with full functionality

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Biography from "./components/Biography/Biography";
 import Map from "./components/ExhibitionMap/ExhibitionMap";
 import Games from "./components/Games/Games";
 import MemoryGame from "./components/MemoryGame/MemoryGame";
-import DragAndDrop from "./components/DragAndDrop/DragAndDrop";
+import Canvas from "./components/Canvas/Canvas";
 import EventList from "./components/EventList/EventList";
 import Impressum from "./components/Impressum /Impressum";
 
@@ -21,7 +21,7 @@ function App() {
           <Route path="/exhibition" element={<Map />} />
           <Route path="/games" element={<Games />} />
           <Route path="/memory" element={<MemoryGame />} />
-          <Route path="/built-your-own-basquiat" element={<DragAndDrop />} />
+          <Route path="/built-your-own-basquiat" element={<Canvas />} />
           <Route path="/events" element={<EventList />} />
           <Route path="/impressum" element={<Impressum />} />
         </Routes>
